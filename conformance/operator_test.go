1d1aace5cafa4d81d797d17f9b063b37f6d92821
Federico Paolinelli <fpaoline@redhat.com>
2020-02-14 18:51:30 +0100

    Add a makefile rule to run conformance tests
    Signed-off-by: Federico Paolinelli <fpaoline@redhat.com>

--- conformance/operator_test.go
+++ conformance/operator_test.go
@@ -2,24 +2,18 @@ package conformance
 
 import (
 	"context"
-	"fmt"
 	"time"
 
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 
 	sriovv1 "github.com/openshift/sriov-network-operator/pkg/apis/sriovnetwork/v1"
+	"github.com/openshift/sriov-tests/pkg/util/cluster"
 	"github.com/openshift/sriov-tests/pkg/util/execute"
 	"github.com/openshift/sriov-tests/pkg/util/namespaces"
-	runtimeclient "sigs.k8s.io/controller-runtime/pkg/client"
-
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	runtimeclient "sigs.k8s.io/controller-runtime/pkg/client"
 )
-
-type sriovEnabledNodes struct {
-	nodes  []string
-	states map[string]sriovv1.SriovNetworkNodeState
-}
 
 var _ = Describe("operator", func() {
 	// var sriovInfos *sriovEnabledNodes
@@ -30,8 +24,8 @@ var _ = Describe("operator", func() {
 		err = namespaces.Clean(namespaces.Test, clients)
 		Expect(err).ToNot(HaveOccurred())
 
-		// sriovInfos, err = discoverSriovOnCluster()
-		// TODO Enable once we need info related to the cluster
+		// This also validates that sriov is properly configured, ignoring the configuration ATM
+		_, err = cluster.DiscoverSriov(clients, operatorNamespace)
 		Expect(err).ToNot(HaveOccurred())
 	})
 
@@ -49,6 +43,7 @@ var _ = Describe("operator", func() {
 				allNodes, err := clients.Nodes().List(metav1.ListOptions{
 					LabelSelector: "node-role.kubernetes.io/worker",
 				})
+				Expect(len(allNodes.Items)).To(BeNumerically(">", 0), "There must be at least one worker")
 				candidate := allNodes.Items[0]
 				candidate.Labels["sriovenabled"] = "true"
 				_, err = clients.Nodes().Update(&candidate)
@@ -74,7 +69,6 @@ var _ = Describe("operator", func() {
 				}, 1*time.Minute, 1*time.Second).Should(Equal(true))
 
 				By("Restoring the node selector for daemons")
-				cfg = sriovv1.SriovOperatorConfig{}
 				err = clients.Get(context.TODO(), runtimeclient.ObjectKey{
 					Name:      "default",
 					Namespace: operatorNamespace,
@@ -110,6 +104,7 @@ func daemonsScheduledOnNodes(selector string) bool {
 				// Removing the element from the list as we want to make sure
 				// the daemons are running on different nodes
 				nodes = append(nodes[:i], nodes[i+1:]...)
+				break
 			}
 		}
 		if !foundNode {
@@ -118,40 +113,4 @@ func daemonsScheduledOnNodes(selector string) bool {
 	}
 	return true
 
-}
-
-func discoverSriovOnCluster() (*sriovEnabledNodes, error) {
-	nodeStates, err := clients.SriovNetworkNodeStates("openshift-sriov-operator").List(metav1.ListOptions{})
-	res := &sriovEnabledNodes{}
-	if err != nil {
-		return nil, fmt.Errorf("Failed to retrieve note states %v", err)
-	}
-	for _, state := range nodeStates.Items {
-		if state.Status.SyncStatus != "Succeeded" {
-			Fail("Sync status still in progress")
-		}
-		node := state.Name
-		for _, itf := range state.Status.Interfaces {
-			if itf.NumVfs > 0 {
-				res.nodes = append(res.nodes, node)
-				res.states[node] = state
-				continue
-			}
-		}
-	}
-	return res, nil
-}
-
-func (n *sriovEnabledNodes) findSriovDevice(node string) (*sriovv1.InterfaceExt, error) {
-	s, ok := n.states[node]
-	if !ok {
-		return nil, fmt.Errorf("Node %s not found", node)
-	}
-	for _, itf := range s.Status.Interfaces {
-		if itf.NumVfs > 0 {
-			return &itf, nil
-		}
-	}
-
-	return nil, fmt.Errorf("Unable to find sriov devices in node %s", node)
 }
